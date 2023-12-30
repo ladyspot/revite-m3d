81d58caa8dadfa8c2b6c01190246b5cee7aafee2
ladyspot <35229381+ladyspot@users.noreply.github.com>
2023-12-30 05:07:51 +0100

    Add custom official and verified badge to Match3D Server

--- src/components/common/ServerHeader.tsx
+++ src/components/common/ServerHeader.tsx
@@ -9,7 +9,6 @@ import { Text } from "preact-i18n";
 
 import { IconButton } from "@revoltchat/ui";
 
-import { modalController } from "../../controllers/modals/ModalController";
 import Tooltip from "./Tooltip";
 
 interface Props {
@@ -24,7 +23,7 @@ const ServerBanner = styled.div<Omit<Props, "server">>`
     justify-content: flex-end;
 
     background-size: cover;
-    background-repeat: norepeat;
+    background-repeat: no-repeat;
     background-position: center center;
 
     ${(props) =>
@@ -70,73 +69,34 @@ const ServerBanner = styled.div<Omit<Props, "server">>`
 
 export default observer(({ server }: Props) => {
     const bannerURL = server.generateBannerURL({ width: 480 });
+    const isOfficialServer = server._id === "01HHVH0ENWJMSHZF37DJH07J0Z";
+    const isVerifiedServer = server.flags && (server.flags & 1); // Adjust this based on how your flags are set up
 
     return (
         <ServerBanner
             background={typeof bannerURL !== "undefined"}
-            style={{
-                backgroundImage: bannerURL ? `url('${bannerURL}')` : undefined,
-            }}>
+            style={{ backgroundImage: bannerURL ? `url('${bannerURL}')` : undefined }}>
             <div className="container">
-                {server.flags && server.flags & 1 ? (
-                    <Tooltip
-                        content={
-                            <Text id="app.special.server-badges.official" />
-                        }
-                        placement={"bottom-start"}>
+                {isOfficialServer && (
+                    <Tooltip content={<Text id="app.special.server-badges.official" />} placement={"bottom-start"}>
                         <svg width="20" height="20">
-                            <image
-                                xlinkHref="/assets/badges/verified.svg"
-                                height="20"
-                                width="20"
-                            />
-                            <image
-                                xlinkHref="/assets/badges/revolt_r.svg"
-                                height="15"
-                                width="15"
-                                x="2"
-                                y="3"
-                                style={
-                                    "justify-content: center; align-items: center; filter: brightness(0);"
-                                }
-                            />
+                            <image xlinkHref="https://panel.match3d.space/badges/official.png" height="20" width="20" />
                         </svg>
                     </Tooltip>
-                ) : undefined}
-                {server.flags && server.flags & 2 ? (
-                    <Tooltip
-                        content={
-                            <Text id="app.special.server-badges.verified" />
-                        }
-                        placement={"bottom-start"}>
+                )}
+                {isOfficialServer && (
+                    <Tooltip content={<Text id="app.special.server-badges.verified" />} placement={"bottom-start"}>
                         <svg width="20" height="20">
-                            <image
-                                xlinkHref="/assets/badges/verified.svg"
-                                height="20"
-                                width="20"
-                            />
-                            <foreignObject x="2" y="2" width="15" height="15">
-                                <Check
-                                    size={15}
-                                    color="black"
-                                    strokeWidth={8}
-                                />
-                            </foreignObject>
+                            <image xlinkHref="https://panel.match3d.space/badges/verified.png" height="20" width="20" />
                         </svg>
                     </Tooltip>
-                ) : undefined}
-                <a
-                    className="title"
-                    onClick={() =>
-                        modalController.push({ type: "server_info", server })
-                    }>
+                )}
+                <a className="title" onClick={() => modalController.push({ type: "server_info", server })}>
                     {server.name}
                 </a>
                 {server.havePermission("ManageServer") && (
                     <Link to={`/server/${server._id}/settings`}>
-                        <IconButton>
-                            <Cog size={20} />
-                        </IconButton>
+                        <IconButton><Cog size={20} /></IconButton>
                     </Link>
                 )}
             </div>
