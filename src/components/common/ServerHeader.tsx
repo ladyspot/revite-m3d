cb2b7557f8a093bfec56401f93065fd5970abe21
ladyspot <35229381+ladyspot@users.noreply.github.com>
2023-12-30 22:29:27 +0100

    Apply pulsating and rainbow to official server

--- src/components/common/ServerHeader.tsx
+++ src/components/common/ServerHeader.tsx
@@ -3,12 +3,10 @@ import { Cog } from "@styled-icons/boxicons-solid";
 import { observer } from "mobx-react-lite";
 import { Link } from "react-router-dom";
 import { Server } from "revolt.js";
-import styled, { css } from "styled-components/macro";
+import styled, { css, createGlobalStyle } from "styled-components/macro";
 
 import { Text } from "preact-i18n";
-
 import { IconButton } from "@revoltchat/ui";
-
 import Tooltip from "./Tooltip";
 
 interface Props {
@@ -16,7 +14,30 @@ interface Props {
     background?: boolean;
 }
 
-const ServerBanner = styled.div<Omit<Props, "server">>`
+// Keyframes for pulsating effect
+const pulsateKeyframes = css`
+    @keyframes pulsate {
+        0% { transform: scale(1); }
+        50% { transform: scale(1.05); }
+        100% { transform: scale(1); }
+    }
+`;
+
+// Keyframes for RGB text effect
+const rainbowTextKeyframes = css`
+    @keyframes rainbowText {
+        0%,100% { filter: hue-rotate(0deg); }
+        50% { filter: hue-rotate(-360deg); }
+    }
+`;
+
+// Global style for keyframes
+const GlobalStyle = createGlobalStyle`
+    ${pulsateKeyframes}
+    ${rainbowTextKeyframes}
+`;
+
+const ServerBanner = styled.div<Omit<Props, "server"> & { isOfficialServer: boolean }>`
     flex-shrink: 0;
     display: flex;
     flex-direction: column;
@@ -63,6 +84,10 @@ const ServerBanner = styled.div<Omit<Props, "server">>`
 
             cursor: pointer;
             color: var(--foreground);
+
+            ${(props) => props.isOfficialServer && css`
+                animation: pulsate 1.5s ease-in-out infinite, rainbowText 6s ease-in-out infinite;
+            `}
         }
     }
 `;
@@ -70,36 +95,39 @@ const ServerBanner = styled.div<Omit<Props, "server">>`
 export default observer(({ server }: Props) => {
     const bannerURL = server.generateBannerURL({ width: 480 });
     const isOfficialServer = server._id === "01HHVH0ENWJMSHZF37DJH07J0Z";
-    const isVerifiedServer = server.flags && (server.flags & 1); // Adjust this based on how your flags are set up
 
     return (
-        <ServerBanner
-            background={typeof bannerURL !== "undefined"}
-            style={{ backgroundImage: bannerURL ? `url('${bannerURL}')` : undefined }}>
-            <div className="container">
-                {isOfficialServer && (
-                    <Tooltip content={<Text id="app.special.server-badges.official" />} placement={"bottom-start"}>
-                        <svg width="20" height="20">
-                            <image xlinkHref="https://panel.match3d.space/badges/official.png" height="20" width="20" />
-                        </svg>
-                    </Tooltip>
-                )}
-                {isOfficialServer && (
-                    <Tooltip content={<Text id="app.special.server-badges.verified" />} placement={"bottom-start"}>
-                        <svg width="20" height="20">
-                            <image xlinkHref="https://panel.match3d.space/badges/verified.png" height="20" width="20" />
-                        </svg>
-                    </Tooltip>
-                )}
-                <a className="title" onClick={() => modalController.push({ type: "server_info", server })}>
-                    {server.name}
-                </a>
-                {server.havePermission("ManageServer") && (
-                    <Link to={`/server/${server._id}/settings`}>
-                        <IconButton><Cog size={20} /></IconButton>
-                    </Link>
-                )}
-            </div>
-        </ServerBanner>
+        <>
+            <GlobalStyle />
+            <ServerBanner
+                background={typeof bannerURL !== "undefined"}
+                style={{ backgroundImage: bannerURL ? `url('${bannerURL}')` : undefined }}
+                isOfficialServer={isOfficialServer}>
+                <div className="container">
+                    {isOfficialServer && (
+                        <Tooltip content={<Text id="app.special.server-badges.official" />} placement={"bottom-start"}>
+                            <svg width="20" height="20">
+                                <image xlinkHref="https://panel.match3d.space/badges/official.png" height="20" width="20" />
+                            </svg>
+                        </Tooltip>
+                    )}
+                    {isOfficialServer && (
+                        <Tooltip content={<Text id="app.special.server-badges.verified" />} placement={"bottom-start"}>
+                            <svg width="20" height="20">
+                                <image xlinkHref="https://panel.match3d.space/badges/verified.png" height="20" width="20" />
+                            </svg>
+                        </Tooltip>
+                    )}
+                    <a className="title" onClick={() => modalController.push({ type: "server_info", server })}>
+                        {server.name}
+                    </a>
+                    {server.havePermission("ManageServer") && (
+                        <Link to={`/server/${server._id}/settings`}>
+                            <IconButton><Cog size={20} /></IconButton>
+                        </Link>
+                    )}
+                </div>
+            </ServerBanner>
+        </>
     );
 });
