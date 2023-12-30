0eda069a58054a31834f41556ca4108814f2034e
ladyspot <35229381+ladyspot@users.noreply.github.com>
2023-12-30 15:51:57 +0100

    Update UserProfile.tsx

--- src/controllers/modals/components/legacy/UserProfile.tsx
+++ src/controllers/modals/components/legacy/UserProfile.tsx
@@ -201,29 +201,21 @@ export const UserProfile = observer(
                                 </span>
                             </div>
                             {user._id === "01HHVGSJ190P0NERR9EP49EFK5" && (
-                        <Tooltip
-                         content={
-                        <Text id="app.special.popovers.user_profile.badges.founder" />
-                        }>
-                        <img src="https://panel.match3d.space/badges/founder.png" />
-                        </Tooltip>
-                            )}
-                         {user._id === "01HHVGSJ190P0NERR9EP49EFK5" && (
-                        <Tooltip
-                         content={
-                        <Text id="app.special.popovers.user_profile.badges.moderation" />
-                        }>
-                        <img src="https://panel.match3d.space/badges/moderator.png" />
-                        </Tooltip>
-                            )}
-                         {user._id === "01HHVGSJ190P0NERR9EP49EFK5" && (
-                        <Tooltip
-                         content={
-                        <Text id="app.special.popovers.user_profile.badges.developer" />
-                        }>
-                        <img src="https://panel.match3d.space/badges/developer.png" />
-                        </Tooltip>
-                            )}
+    <Tooltip content={<Text id="app.special.popovers.user_profile.badges.founder" />}>
+        <img src="https://panel.match3d.space/badges/founder.png" />
+    </Tooltip>
+)}
+{user._id === "01HHVGSJ190P0NERR9EP49EFK5" && (
+    <Tooltip content={<Text id="app.special.popovers.user_profile.badges.moderation" />}>
+        <img src="https://panel.match3d.space/badges/moderator.png" />
+    </Tooltip>
+)}
+{user._id === "01HHVGSJ190P0NERR9EP49EFK5" && (
+    <Tooltip content={<Text id="app.special.popovers.user_profile.badges.developer" />}>
+        <img src="https://panel.match3d.space/badges/developer.png" />
+    </Tooltip>
+)}
+
                             {user.status?.text && (
                                 <span className={styles.status}>
                                     <UserStatus user={user} tooltip />
