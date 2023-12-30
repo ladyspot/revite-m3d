ef9c341ed8df6312e5fd805967db63073e23024c
ladyspot <35229381+ladyspot@users.noreply.github.com>
2023-12-30 04:31:29 +0100

    Second try, display of badges

--- src/components/common/ServerHeader.tsx
+++ src/components/common/ServerHeader.tsx
@@ -23,7 +23,7 @@ const ServerBanner = styled.div<Omit<Props, "server">>`
     justify-content: flex-end;
 
     background-size: cover;
-    background-repeat: norepeat;
+    background-repeat: no-repeat;
     background-position: center center;
 
     ${(props) =>
@@ -85,21 +85,17 @@ export default observer(({ server }: Props) => {
                         content={<Text id="app.special.server-badges.official" />}
                         placement={"bottom-start"}>
                         <svg width="20" height="20">
-                            // Your badge SVG or other elements go here
                             <image
-                                xlinkHref="/path/to/official-badge.svg"
+                                xlinkHref="/assets/badges/revolt_r.svg"
                                 height="20"
                                 width="20"
                             />
-                            
                         </svg>
                     </Tooltip>
                 )}
                 {server.flags && server.flags & 1 ? (
                     <Tooltip
-                        content={
-                            <Text id="app.special.server-badges.verified" />
-                        }
+                        content={<Text id="app.special.server-badges.verified" />}
                         placement={"bottom-start"}>
                         <svg width="20" height="20">
                             <image
@@ -107,38 +103,6 @@ export default observer(({ server }: Props) => {
                                 height="20"
                                 width="20"
                             />
-                            <image
-                                xlinkHref="/assets/badges/revolt_r.svg"
-                                height="15"
-                                width="15"
-                                x="2"
-                                y="3"
-                                style={
-                                    "justify-content: center; align-items: center; filter: brightness(0);"
-                                }
-                            />
-                        </svg>
-                    </Tooltip>
-                ) : undefined}
-                {server.flags && server.flags & 2 ? (
-                    <Tooltip
-                        content={
-                            <Text id="app.special.server-badges.verified" />
-                        }
-                        placement={"bottom-start"}>
-                        <svg width="20" height="20">
-                            <image
-                                xlinkHref="/assets/badges/verified.svg"
-                                height="20"
-                                width="20"
-                            />
-                            <foreignObject x="2" y="2" width="15" height="15">
-                                <Check
-                                    size={15}
-                                    color="black"
-                                    strokeWidth={8}
-                                />
-                            </foreignObject>
                         </svg>
                     </Tooltip>
                 ) : undefined}
